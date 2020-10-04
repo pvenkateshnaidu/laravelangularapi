aabc69f141b69398a9c92460646ec76d6209081d
venkatesh <pvenkateshnaidu@gmail.com>
2020-10-04 12:34:01 +0530

    Added extra fields admin documents

--- app/Http/Controllers/Admin/AdminDocumentsController.php
+++ app/Http/Controllers/Admin/AdminDocumentsController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 use App\TimeSheetDocuments;
 use Carbon\Carbon;
 use Auth;
+class myObject
+{
+    public $documentId;
+    public $dateOfWeek;
+    public $name;
+    public $doucmentName;
+    public $startWeek;
+    public $endWeek;
+}
 class AdminDocumentsController extends Controller
 {
     public function __construct()
@@ -21,10 +30,27 @@ class AdminDocumentsController extends Controller
      */
     public function index()
     {
-        //
+
 
         $timesheet = TimeSheetDocuments::with('user_details')->orderBy('dateOfWeek', 'ASC')
         ->get();
-        return response()->json(['documents' => $timesheet ], 200);
+
+
+        $MyObjects = array();
+
+        foreach($timesheet as $value)
+        {
+            $MyObject = new myObject;
+            $MyObject->documentId =$value->documentId;
+            $MyObject->dateOfWeek =$value->dateOfWeek;
+            $MyObject->name=$value->user_details->name;
+            $MyObject->doucmentName =$value->doucmentName;
+            $from= Carbon::parse($value->dateOfWeek)->startOfWeek(Carbon::SUNDAY)->format('Y-m-d');
+            $to= Carbon::parse($value->dateOfWeek)->endOfWeek(Carbon::SATURDAY)->format('Y-m-d');
+            $MyObject->startWeek =$from;
+            $MyObject->endWeek =$to;
+            $MyObjects[] = $MyObject;
+        }
+        return response()->json(['documents' => $MyObjects ], 200);
     }
 }
